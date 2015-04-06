ffcd80ea9208093800fded47ef75f74c90f01969
rinrin <boo@saltycandy.net>
2015-04-06 13:15:59 +0900

    Add a full_title helper

---
+++ vendor/bundle/ruby/2.0.0/gems/pg-0.15.1/ext/siteconf20150406-871-k8dtu.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20150406-871-o8ee5g"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
